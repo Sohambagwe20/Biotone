fe771b4afa50ff4fc74d2e378237ae5456f876b7
Sohambagwe20 <bagwesoham20@gmail.com>
2026-02-24 13:15:34 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -372,9 +372,8 @@ def reset_password(email):
     return render_template('reset_password.html', email=email)
 
 with app.app_context():
-    db.drop_all()
     db.create_all()
-
+    
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
